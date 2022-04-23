1963dda793ba890b5d6b6c5aa0f84ebd9cfd9362
trust0212 <sergeyfedorenkodev@outlook.com>
2022-04-23 10:07:26 -0400

    feat: Fix block explorer link

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,7 @@ BigNumber.config({
 export const BSC_BLOCK_TIME = 3
 
 export const BASE_BSC_SCAN_URLS = {
-  [ChainId.MAINNET]: 'https://blockscout.com/astra',
+  [ChainId.MAINNET]: 'https://blockscout.com/astar',
   [ChainId.TESTNET]: 'https://testnet.bscscan.com',
 }
 
